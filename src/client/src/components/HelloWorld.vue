88c4e02f409c3a41dd815a0e08ed4fb3e57d95b5
Finn Kockelke <finn_kockelke@gmx.net>
2025-09-04 00:09:09 +0200

    chore: add vue

---
+++ src/client/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script language="ts" setup>
+import axios from "axios";
+import { onMounted, ref } from "vue";
+
+const message = ref("");
+
+onMounted(async () => {
+  try {
+    const response = await axios.get(`http://localhost:3000/api/message`);
+    message.value = response.data.message;
+  } catch (error) {
+    console.error("Error fetching message:", error);
+  }
+});
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
+import cors from "cors";
 import express from "express";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = process.env.PORT ?? "9001";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Serve static files from the Vue app
+app.use(express.static(path.join(__dirname, "..", "dist", "client")));
+
+// Redirect all requests to the index.html
 app.get("/", (req, res) => {
-  console.log(`Connection at ${new Date().toISOString()}`);
-  res.send("Hello World! This is a test server.");
+  res.sendFile(path.join(__dirname, "..", "dist", "client", "index.html"));
 });
 
+// Api routes
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from the server!" });
+});
+
+// Start server
 app.listen(port, () => {
   console.log(`Node server listening on port ${port}`);
 });
