76e4a914096cc327108d27df92cbb63d3d3cd83c
Finn Kockelke <finn_kockelke@gmx.net>
2025-09-04 00:21:05 +0200

    chore: satisfy linter

--- src/client/src/router/index.ts
+++ src/client/src/router/index.ts
@@ -1,30 +1,32 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+
 import HomeView from "../views/HomeView.vue";
 
-const routes: Array<RouteRecordRaw> = [
+const routes: RouteRecordRaw[] = [
   {
-    path: "/",
-    name: "home",
     component: HomeView,
+    name: "home",
+    path: "/",
   },
   {
-    path: "/about",
-    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    name: "about",
+    path: "/about",
   },
   {
-    path: "/project1",
-    name: "Project 1",
     component: () => import("../views/Project1View.vue"),
+    name: "Project 1",
+    path: "/project1",
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+  history: createWebHistory(process.env.BASE_URL ?? "/"),
   routes,
 });
 
